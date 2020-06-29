f59b38d6244ef79e8bf89c8c575ff9512324efbf
Vladislav Kantaev <vvkantaev@yandex.ru>
2020-06-29 12:10:56 +0300

    Initial commit

---
+++ Assets/Plugins/Validation/ComponentValidationError.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Object = UnityEngine.Object;
+
+namespace Validation
+{
+    public sealed class ComponentValidationError : Exception
+    {
+        public override string Message { get; }
+
+        public ComponentValidationError(Object context, Type componentType)
+        {
+            Message = $"Component of type {componentType.Name} was not found in {context.name}.";
+        }
+    }
+}
